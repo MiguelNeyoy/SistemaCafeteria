4c46aa3e1a3d4a23ee63598824e83fede66529ad
dev-jesrod <rodolfolizr19@gmail.com>
2026-07-10 10:39:36 -0700

    Creando el ViewModel MainViewModel.cs
    Siguiendo la orientacion de la IA, este archivo es el cerebro principal
    que permite decidir que es lo que visualizara al momento de dar click
    en este caso a los botones.

---
+++ Presentation.WPF/ViewModels/MainViewModel.cs
@@ -0,0 +1,10 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System.Collections.ObjectModel;
+
+namespace Presentation.WPF.ViewModels;
+
+public partial class MainViewModel : ObservableObject
+{
+    
+}
