7c99d18376486ff9e28ca9a3e0d5c581ecc0b3b0
dev-jesrod <rodolfolizr19@gmail.com>
2026-07-23 12:37:03 -0700

    feat: Añadiendo archivo .cs de Comanda

---
+++ Presentation.WPF/Views/Comanda.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Presentation.WPF.Views
+{
+    /// <summary>
+    /// Lógica de interacción para Comanda.xaml
+    /// </summary>
+    public partial class Comanda : UserControl
+    {
+        public Comanda()
+        {
+            InitializeComponent();
+        }
+    }
+}
