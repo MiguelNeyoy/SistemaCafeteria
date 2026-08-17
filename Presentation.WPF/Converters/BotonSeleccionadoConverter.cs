58a5c469473fbe31d76d06ef3f237cfa7aadf880
dev-jesrod <rodolfolizr19@gmail.com>
2026-08-17 13:10:12 -0700

    feat: Se crea el archivo, con el proposito de que la vista sepa que boton debe de mostrar.

---
+++ Presentation.WPF/Converters/BotonSeleccionadoConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Windows.Data;
+using System.Globalization;
+
+namespace Presentation.WPF.Converters
+{
+    public class BotonSeleccionadoConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == parameter)
+            {
+                return true;
+
+            }else 
+                return false;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
