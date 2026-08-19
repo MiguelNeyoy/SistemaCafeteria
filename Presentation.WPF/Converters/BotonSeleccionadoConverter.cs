b143190232f432eab521cff6adf67cc6d2aae0c3
dev-jesrod <rodolfolizr19@gmail.com>
2026-08-18 20:41:25 -0700

    feat: Cambiamos IValueConverter por IMultiValueConverter.

--- Presentation.WPF/Converters/BotonSeleccionadoConverter.cs
+++ Presentation.WPF/Converters/BotonSeleccionadoConverter.cs
@@ -4,21 +4,21 @@ using System.Globalization;
 
 namespace Presentation.WPF.Converters
 {
-    public class BotonSeleccionadoConverter : IValueConverter
+    public class BotonSeleccionadoConverter : IMultiValueConverter
     {
-        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
         {
-            if (value == parameter)
+            if (values[0] == values[1])
             {
                 return true;
-
-            }else 
+            }
+            else
                 return false;
         }
 
-        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
         {
-            throw new NotImplementedException();
+            throw new NotSupportedException();
         }
     }
 }
