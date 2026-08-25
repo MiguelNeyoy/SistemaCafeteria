a90d6dec8ea941133622f2f9fcb40aecffe6f4f4
dev-jesrod <rodolfolizr19@gmail.com>
2026-08-24 19:47:41 -0700

    feat: Agregamos la infraestructura necesaria.

--- Presentation.WPF/App.xaml.cs
+++ Presentation.WPF/App.xaml.cs
@@ -1,12 +1,33 @@
-﻿using System.Windows;
-using Core.Application;
+﻿using Core.Application;
 using Infrastructure.Persistence;
 using Microsoft.Extensions.DependencyInjection;
+using Presentation.WPF;
+using Presentation.WPF.ViewModels;
+using System.Windows;
 
 /// <summary>
 /// Interaction logic for App.xaml
 /// </summary>
 public partial class App : Application
 {
-}
+    private ServiceProvider _serviceProvider;
 
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        base.OnStartup(e);
+
+        var services = new ServiceCollection();
+
+        services.AddApplication();
+        services.AddPersistence();
+
+        services.AddTransient<MainWindow>();
+        services.AddTransient<MainViewModel>();
+
+        _serviceProvider = services.BuildServiceProvider();
+
+        var mainWindow = _serviceProvider.GetRequiredService<MainWindow>();
+
+        mainWindow.Show();
+    }
+}
