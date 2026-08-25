15d4f70127d897027f8c82e8fff57fd4dfe3e6fb
dev-jesrod <rodolfolizr19@gmail.com>
2026-08-24 19:45:52 -0700

    feat: Agregamos las rutas necesarias para empezar el punto de configuracion.

--- Presentation.WPF/App.xaml.cs
+++ Presentation.WPF/App.xaml.cs
@@ -1,8 +1,7 @@
-﻿using System.Configuration;
-using System.Data;
-using System.Windows;
-
-namespace Presentation.WPF;
+﻿using System.Windows;
+using Core.Application;
+using Infrastructure.Persistence;
+using Microsoft.Extensions.DependencyInjection;
 
 /// <summary>
 /// Interaction logic for App.xaml
