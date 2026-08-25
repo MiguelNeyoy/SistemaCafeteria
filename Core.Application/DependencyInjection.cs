2809c4a525f9e2cbdbc2e966445924e88664ba06
dev-jesrod <rodolfolizr19@gmail.com>
2026-08-24 19:42:28 -0700

    feat: Agregamos la infraestructura necesaria del Dependecy Injection.

---
+++ Core.Application/DependencyInjection.cs
@@ -0,0 +1,17 @@
+﻿using Core.Application.Interfaces.Services;
+using Core.Application.UseCases;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Core.Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplication(
+        this IServiceCollection services)
+    {
+        services.AddScoped<ICategoriaService, CategoriaService>();
+        services.AddScoped<IProductoService, ProductoService>();
+
+        return services;
+    }
+}
