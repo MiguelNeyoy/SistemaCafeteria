706edd7da0be1c3ed44d59e2c43da109fbd99470
dev-jesrod <rodolfolizr19@gmail.com>
2026-08-29 11:11:10 -0700

    feat: conectar categorias de MenuView con servicio

--- Presentation.WPF/ViewModels/MenuViewModel.cs
+++ Presentation.WPF/ViewModels/MenuViewModel.cs
@@ -1,6 +1,8 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using System.Collections.ObjectModel;
+using Core.Application.Dtos.Catalogo;
+using Core.Application.Interfaces.Services;
 
 namespace Presentation.WPF.ViewModels;
 
@@ -8,11 +10,15 @@ public partial class MenuViewModel : ObservableObject
 {
     public ObservableCollection<CategoriaItem> Categorias { get; } = new();
 
+    private readonly ICategoriaService _categoriaService;
+
     [ObservableProperty]
     private bool tieneCategorias;
 
 
-    public MenuViewModel() {
+    public MenuViewModel( ICategoriaService categoriaService ) 
+    {
+        _categoriaService = categoriaService;
 
         Categorias.CollectionChanged += (_, _) =>
         {
@@ -25,6 +31,24 @@ public partial class MenuViewModel : ObservableObject
 
 public class CategoriaItem
 {
+    public int Id { get; set; }
     public string Nombre { get; set; } = string.Empty;
     public string Imagen { get; set; } = string.Empty;
 }
+
+
+public async Task CargarCategoriasAsync()
+    {
+        var categorias = await _categoriaService.ObtenerActivasAsync();
+
+        Categorias.Clear();
+
+        foreach (var categoria in categorias)
+        {
+            Categorias.Add(new CategoriaItem
+            {
+                Id = categoria.Id,
+                Nombre = categoria.Nombre
+            });
+        }
+    }
